ec5ba8d78faea5023212ad4e6e07a575dcd38279
Jose Garfias Lopez <jose@karmapulse.com>
2018-07-31 11:58:45 -0500

    removed for dojo

--- react_portals_fragments/src/App.js
+++ react_portals_fragments/src/App.js
@@ -2,48 +2,13 @@ import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 
-class Gnome extends Component {
-  constructor(props) {
-    super(props);
-    this.container = document.createElement('div');
-    document.body.appendChild(this.container);
-  }
-
-  componentWillUnmount() {
-    document.body.removeChild(this.container);
-  }
-
-  
-  render() {
-    return ReactDOM.createPortal(
-              <Fragment>
-                < div className = "gnome" > < /div> 
-                <button > ESCONDER! < /button>
-                </Fragment>,
-                this.container
-            );
-  }
-}
-
-
 class App extends Component {
-
-  state = { showGnome: false }
-
-  toggleGnome = ()=> { this.setState({ showGnome: !this.state.showGnome })}
-
   render() {
     return (
-      <Fragment>
-        <h1> React demo </h1> 
-        <div className = "house" onClick={this.toggleGnome}>
-          <button> ESCONDER </button>
-          {
-            this.state.showGnome && <Gnome/>
-          }
-        </div>
-        
-      </Fragment>
+      <div>
+        <h1> React portals and fragments </h1>
+        <div className="house"> </div>
+      </div>
     );
   }
 }
