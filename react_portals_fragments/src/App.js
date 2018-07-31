1713c1842aed3252efb912dde1221f409b1dad6b
Jose Garfias Lopez <jose@karmapulse.com>
2018-07-31 12:45:24 -0500

    finished exercise

--- react_portals_fragments/src/App.js
+++ react_portals_fragments/src/App.js
@@ -2,13 +2,41 @@ import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 
+class Gnome extends Component {
+  constructor(props) {
+    super(props);
+    this.container = document.createElement('div');
+    document.body.appendChild(this.container);
+  }
+  componentWillUnmount() {
+    document.body.removeChild(this.container);
+  }
+  render() {
+    return ReactDOM.createPortal(
+      <Fragment>
+        <div className="gnome">
+          <button> ESCONDER </button>
+        </div>
+      </Fragment>,
+      this.container
+    )
+  }
+}
+
 class App extends Component {
+  state = { showGnome: false }
+  toogleGnome = () => { this.setState({showGnome: !this.state.showGnome })}
   render() {
     return (
-      <div>
+      <Fragment>
         <h1> React portals and fragments </h1>
-        <div className="house"> </div>
-      </div>
+        <div className="house" onClick={this.toogleGnome}>
+          <button> ESCONDER </button>
+          {
+            this.state.showGnome ? <Gnome /> : <div className="gnome"></div>
+          }
+        </div>
+      </Fragment>
     );
   }
 }
